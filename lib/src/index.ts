8c7932cdd8fa60baa4f51c417a755305ee319435
Matheson Mawhinney <matheson.mawhinney@gmail.com>
2018-11-19 13:40:49 -0800

    test package reording

---
+++ lib/src/index.ts
@@ -0,0 +1,6 @@
+export { McBreadcrumbsComponent } from './component/mc-breadcrumbs.component'
+export { McBreadcrumbsService } from './service/mc-breadcrumbs.service';
+export { McBreadcrumbsConfig } from './service/mc-breadcrumbs.config';
+export { IBreadcrumb } from './mc-breadcrumbs.shared';
+export { McBreadcrumbsResolver } from './service/mc-breadcrumbs.resolver'
+export { McBreadcrumbsModule } from './mc-breadcrumbs.module';
